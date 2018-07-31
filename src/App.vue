1de9c6af5ebced0dafe824c33371f0900be6a71d
mkyoon <mkyoon14@gmail.com>
2018-07-31 15:15:07 +0900

    removed the others components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <spec-picker/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import SpecPicker from './components/SpecPicker.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     SpecPicker
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
